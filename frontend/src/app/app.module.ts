d19b8b442caec0fc4b5153b561c0a31184c6e495
Nishikantaray <nishikantaray1@gmail.com>
2022-06-01 00:57:39 +0530

    added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/home/home.component';
+import { NavComponent } from './main/nav/nav.component';
+import { BannerComponent } from './main/banner/banner.component';
+import { WhychooseComponent } from './main/whychoose/whychoose.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { TestimonialComponent } from './main/testimonial/testimonial.component';
+import { FunfactorComponent } from './main/funfactor/funfactor.component';
+import { LiveexamComponent } from './main/liveexam/liveexam.component';
+import { LiveexamheaderComponent } from './main/liveexamheader/liveexamheader.component';
+import { LiveexamcourseComponent } from './main/liveexamcourse/liveexamcourse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    BannerComponent,
+    WhychooseComponent,
+    FooterComponent,
+    TestimonialComponent,
+    FunfactorComponent,
+    LiveexamComponent,
+    LiveexamheaderComponent,
+    LiveexamcourseComponent
   ],
   imports: [
     BrowserModule,
